bf564ffaf367aa94e0241cbe3c9bbf638d031d0d
Gaju Sandra <gajusandra872@gmail.com>
2026-09-09 11:56:43 +0200

    the mortgage calculator

---
+++ backend cencept/java/javareminder/src/student.java
@@ -0,0 +1,9 @@
+public class student {
+
+String name="yamin";
+int age= 10;
+public  void show_details(){
+    System.out.println("student name is "+name);
+    System.out.println("student age is " +age);
+}
+}
---
+++ backend cencept/java/moertgage calculator/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+
+
+
+        System.out.println("Principal: ");
+        int principal=scanner.nextInt();
+        System.out.println("Annual Interest Rate: ");
+        float interest=scanner.nextFloat();
+        System.out.println("Period (Years): " );
+        int year= scanner.nextInt();
+        double mortgage = principal *
+                (interest * Math.pow(1 + interest, year))
+                / (Math.pow(1 + interest, year) - 1);
+        System.out.println("Mortagage: "+mortgage);
+    }
+}
