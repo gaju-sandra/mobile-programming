f450748f07ba326c2cf22ebea9fc5f65fb829891
Gaju Sandra <gajusandra872@gmail.com>
2026-09-10 11:37:09 +0200

    learning

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -2,12 +2,15 @@ import 'package:cli/cli.dart' as cli;
 
 const version = '0.1.0';
 void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage(); // Change this from 'Hello, Dart!'
-  } else if (arguments.first == 'version') {
+  if(arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  }else if (arguments.first == 'version'){
     print('Dartpedia CLI version $version');
-  } else {
-    printUsage(); // Catch-all for any unrecognized command.
+  }else{
+    printUsage();
   }
-}
 
+}
+void printUsage(){
+  print("The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
+}
