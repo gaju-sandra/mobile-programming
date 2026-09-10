b9baa18ae3f9c58b19efd731a62a7fe8fc1854f4
Gaju Sandra <gajusandra872@gmail.com>
2026-09-10 11:54:55 +0200

    learning

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,4 +1,5 @@
 import 'package:cli/cli.dart' as cli;
+import 'dart:io';
 
 const version = '0.1.0';
 void main(List<String> arguments) {
@@ -7,7 +8,7 @@ void main(List<String> arguments) {
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
   } else if (arguments.first == 'search') {
- 
+
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
     searchWikipedia(inputArgs);
   } else {
@@ -16,9 +17,24 @@ void main(List<String> arguments) {
 }
 
 
-void searchWikipedia(List<String>? arguments){
-  print('searchWikipedia received arguments: $arguments');
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  // If the user didn't pass in arguments, request an article title.
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    // Await input and provide a default empty string if the input is null.
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    // Otherwise, join the arguments into a single string.
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
 }
+
 
 void printUsage(){
   print("The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
